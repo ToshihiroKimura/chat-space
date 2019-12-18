593ac70d3b44d44fb11db8e85a5f68561c41a6c0
ToshihiroKimura <toshi.kimura5903@gmail.com>
2019-12-18 23:57:14 +0900

    define-action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,27 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+ 
   def new
+    @group = Group.new
   end
 
   def create
+    Group.create(group_params)
   end
 
   def edit
   end
 
   def update
+    group.update(group_params)
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
+ 
+  def set_group
+    group = Group.find(params[:id])
   end
 end
