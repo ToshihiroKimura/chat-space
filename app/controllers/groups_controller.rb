fd25f7c09be9e59cbcd4ad4dc56cf7626593c2ef
ToshihiroKimura <toshi.kimura5903@gmail.com>
2019-12-21 20:45:44 +0900

    グループ機能実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,31 +1,41 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
- 
-  def new
-    @group = Group.new
+  
+  def index
+    @groups = current_user.groups
   end
 
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
+  
   def create
-    Group.create(group_params)
-    @group.save
-    redirect_to root_path
+    @group = Group.create(group_params)
+    if @group.save
+        redirect_to root_path, notice: "新規グループを作成しました"
+      else
+        render :new
+      end
   end
 
   def edit
   end
 
   def update
-    Group.update(group_params)
-    redirect_to root_path
+    if @group.update(group_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
-    params
-      .require(:group).permit(Group::name + Groups_user::user)
+    params.require(:group).permit(:name, {user_ids: []})
   end
  
   def set_group
-    group = Group.find(params[:id])
+    @group = Group.find(params[:id])
   end
 end
